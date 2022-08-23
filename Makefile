dca7e86837a164216a8023c2b14ec38191a0c555
start1810 <striganovartem@gmail.com>
2022-08-24 00:56:09 +0300

    step 3

--- Makefile
+++ Makefile
@@ -11,4 +11,4 @@ publish:
 	poetry publish --dry-run
 
 package-install:
-	python3 -m pip install --user dist/*.whl
+	python3 -m pip install --user dist/hexlet_code-0.1.0-py3-none-any.whl
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python3
+
+import prompt
+from .. import cli
+
+
+
 def main():
-    print('Welcome to the Brain Games!')
+    cli.welcome_user()
+   
 
 if __name__ == '__main__':
     main()
